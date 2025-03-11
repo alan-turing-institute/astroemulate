506f6cdbedbe24bad030c27a264df34a62f71198
Ed Chalstrey <edwardchalstrey@gmail.com>
2025-03-11 13:38:22 +0000

    Revert "update url site"
    This reverts commit c975d7ad764a1cb8436a5933671636068a3fdf61.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://alan-turing-institute.github.io/astroemulate',
+    site: 'https://alan-turing-institute.github.io',
+    // base: '/astroemulate',
 })
