489aff1f2c58206c6bdc79fe3540915baa050795
Ed Chalstrey <edwardchalstrey@gmail.com>
2025-03-11 13:16:02 +0000

    comment out base

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://alan-turing-institute.github.io',
-    base: '/astroemulate',
+    // base: '/astroemulate',
 })
