bfd9a458999ae33e9d5efbbae3743456bab3f806
Ed Chalstrey <edwardchalstrey@gmail.com>
2025-03-13 13:30:04 +0000

    configure markdown theme to catppuccin-macchiato

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-// import prism from '@astrojs/prism';
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://alan-turing-institute.github.io',
     base: '/astroemulate',
+    markdown: {
+        shikiConfig: {
+            theme: 'catppuccin-macchiato',
+        },
+    },
 })
