c52dacd6681615cd690f6afdfab3c58e6e23b4c3
Ed Chalstrey <edwardchalstrey@gmail.com>
2025-03-14 14:36:27 +0000

    restore GH site

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://autoemulate.co.uk',
+    site: 'https://alan-turing-institute.github.io',
     base: '/astroemulate',
     markdown: {
         shikiConfig: {
