f69638a4a884e5d46f5f133438fbe4857f3ec796
Ed Chalstrey <edwardchalstrey@gmail.com>
2025-03-14 15:21:33 +0000

    remove base

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://autoemulate.com',
-    base: '/astroemulate',
     markdown: {
         shikiConfig: {
             theme: 'everforest-light',
