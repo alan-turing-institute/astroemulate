1ee1000c4f245a1b27d73c04d63b088ba394529e
Ed Chalstrey <edwardchalstrey@gmail.com>
2025-04-28 16:27:21 +0100

    blog list shows

---
+++ src/content.config.mjs
@@ -0,0 +1,11 @@
+import { defineCollection, z } from 'astro:content';
+
+const blog = defineCollection({
+    schema: z.object({
+        title: z.string(),
+        pubDate: z.preprocess((val) => new Date(val), z.date()),
+        description: z.string().optional(),
+    }),
+});
+
+export const collections = { blog };
